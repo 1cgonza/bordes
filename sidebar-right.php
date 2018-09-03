fd875e2639137cbf4fa2c132152851bc11695740
Juan Camilo González <info@juancgonzalez.com>
2018-09-02 19:03:56 -0500

    init

---
+++ sidebar-right.php
@@ -0,0 +1,5 @@
+	<?php if ( is_active_sidebar( 'popocaca-sidebar' ) ) { ?>
+	<div id="primary_sidebar" class="primary-sidebar widget-area" role="complementary">
+		<?php dynamic_sidebar( 'popocaca-sidebar' ); ?>
+	</div>
+<?php } ?>
