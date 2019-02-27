a279f5899c1938f9ac0c2f7016084910653998e4
Juan Camilo González <info@juancgonzalez.com>
2019-02-26 22:32:57 -0500

    clean metaboxes

--- functions/metaboxes.php
+++ functions/metaboxes.php
@@ -1,364 +1,62 @@
 <?php
 
-if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
-	require_once dirname( __FILE__ ) . '/cmb2/init.php';
-} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
-	require_once dirname( __FILE__ ) . '/CMB2/init.php';
+if (file_exists(dirname(__FILE__) . '/cmb2/init.php')) {
+  require_once dirname(__FILE__) . '/cmb2/init.php';
+} elseif (file_exists(dirname(__FILE__) . '/CMB2/init.php')) {
+  require_once dirname(__FILE__) . '/CMB2/init.php';
 }
 
-add_action( 'cmb2_admin_init', 'bordes_register_demo_metabox' );
+function bordes_register_metabox() {
+  $prefix = 'bordes_';
 
-function bordes_register_demo_metabox() {
-	$prefix = 'bordes_';
+  $bordesMeta = new_cmb2_box(array(
+    'id' => $prefix . 'project',
+    'title' => esc_html__('Metadata', 'cmb2'),
+    'object_types' => array('post'),
+    'context' => 'normal',
+    'priority' => 'high'
+  ));
 
-	$cmb_demo = new_cmb2_box( array(
-		'id'            => $prefix . 'metabox',
-		'title'         => esc_html__( 'Test Metabox', 'cmb2' ),
-		'object_types'  => array( 'post' ), // Post type
-		// 'show_on_cb' => 'bordes_show_if_front_page', // function should return a bool value
-		// 'context'    => 'normal',
-		// 'priority'   => 'high',
-		// 'show_names' => true, // Show field names on the left
-		// 'cmb_styles' => false, // false to disable the CMB stylesheet
-		// 'closed'     => true, // true to keep the metabox closed by default
-		// 'classes'    => 'extra-class', // Extra cmb2-wrap classes
-		// 'classes_cb' => 'bordes_add_some_classes', // Add classes through a callback.
-	) );
+  $bordesMeta->add_field(array(
+    'name' => esc_html__('oEmbed', 'cmb2'),
+    'desc' => sprintf(
+      esc_html__('Enter a youtube, twitter, or instagram URL. Supports services listed at %s.', 'cmb2'),
+      '<a href="https://codex.wordpress.org/Embeds">codex.wordpress.org/Embeds</a>'
+    ),
+    'id' => $prefix . 'embed',
+    'type' => 'oembed',
+  ));
 
-	$cmb_demo->add_field( array(
-		'name'       => esc_html__( 'Test Text', 'cmb2' ),
-		'desc'       => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'         => $prefix . 'text',
-		'type'       => 'text',
-		'show_on_cb' => 'bordes_hide_if_no_cats', // function should return a bool value
-		// 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
-		// 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
-		// 'on_front'        => false, // Optionally designate a field to wp-admin only
-		// 'repeatable'      => true,
-		// 'column'          => true, // Display field value in the admin post-listing columns
-	) );
+  $bordesMeta->add_field(array(
+    'name' => 'Participant',
+    'desc' => 'Drag posts from the left column to the right column to attach them to this page.<br />You may rearrange the order of the posts in the right column by dragging and dropping.',
+    'id' => $prefix . 'participant',
+    'type' => 'custom_attached_posts',
+    'column' => true, // Output in the admin post-listing as a custom column. https://github.com/CMB2/CMB2/wiki/Field-Parameters#column
+    'options' => array(
+      'show_thumbnails' => true, // Show thumbnails on the left
+      'filter_boxes' => true, // Show a text box for filtering the results
+      'query_args' => array(
+        'posts_per_page' => -1,
+        'post_type' => 'participants',
+      ),
+    ),
+  ));
 
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Text Medium', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'textmedium',
-		'type' => 'text_medium',
-	) );
+  $participants = new_cmb2_box(array(
+    'id' => $prefix . 'participant',
+    'title' => esc_html__('Metadata', 'cmb2'),
+    'object_types' => array('participants'),
+    'context' => 'normal',
+    'priority' => 'high',
+  ));
 
-	$cmb_demo->add_field( array(
-		'name'       => esc_html__( 'Read-only Disabled Field', 'cmb2' ),
-		'desc'       => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'         => $prefix . 'readonly',
-		'type'       => 'text_medium',
-		'default'    => esc_attr__( 'Hey there, I\'m a read-only field', 'cmb2' ),
-		'save_field' => false, // Disables the saving of this field.
-		'attributes' => array(
-			'disabled' => 'disabled',
-			'readonly' => 'readonly',
-		),
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Website URL', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'url',
-		'type' => 'text_url',
-		// 'protocols' => array('http', 'https', 'ftp', 'ftps', 'mailto', 'news', 'irc', 'gopher', 'nntp', 'feed', 'telnet'), // Array of allowed protocols
-		// 'repeatable' => true,
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Text Email', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'email',
-		'type' => 'text_email',
-		// 'repeatable' => true,
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Time', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'time',
-		'type' => 'text_time',
-		// 'time_format' => 'H:i', // Set to 24hr format
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Time zone', 'cmb2' ),
-		'desc' => esc_html__( 'Time zone', 'cmb2' ),
-		'id'   => $prefix . 'timezone',
-		'type' => 'select_timezone',
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Date Picker', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'textdate',
-		'type' => 'text_date',
-		// 'date_format' => 'Y-m-d',
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Date Picker (UNIX timestamp)', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'textdate_timestamp',
-		'type' => 'text_date_timestamp',
-		// 'timezone_meta_key' => $prefix . 'timezone', // Optionally make this field honor the timezone selected in the select_timezone specified above
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Date/Time Picker Combo (UNIX timestamp)', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'datetime_timestamp',
-		'type' => 'text_datetime_timestamp',
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Money', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'textmoney',
-		'type' => 'text_money',
-		// 'before_field' => '£', // override '$' symbol if needed
-		// 'repeatable' => true,
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'    => esc_html__( 'Test Color Picker', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'      => $prefix . 'colorpicker',
-		'type'    => 'colorpicker',
-		'default' => '#ffffff',
-		// 'options' => array(
-		// 	'alpha' => true, // Make this a rgba color picker.
-		// ),
-		// 'attributes' => array(
-		// 	'data-colorpicker' => json_encode( array(
-		// 		'palettes' => array( '#3dd0cc', '#ff834c', '#4fa2c0', '#0bc991', ),
-		// 	) ),
-		// ),
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Text Area', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'textarea',
-		'type' => 'textarea',
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Text Area Small', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'textareasmall',
-		'type' => 'textarea_small',
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Text Area for Code', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'textarea_code',
-		'type' => 'textarea_code',
-		// 'attributes' => array(
-		// 	// Optionally override the code editor defaults.
-		// 	'data-codeeditor' => json_encode( array(
-		// 		'codemirror' => array(
-		// 			'lineNumbers' => false,
-		// 			'mode' => 'css',
-		// 		),
-		// 	) ),
-		// ),
-		// To keep the previous formatting, you can disable codemirror.
-		// 'options' => array( 'disable_codemirror' => true ),
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Title Weeeee', 'cmb2' ),
-		'desc' => esc_html__( 'This is a title description', 'cmb2' ),
-		'id'   => $prefix . 'title',
-		'type' => 'title',
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'             => esc_html__( 'Test Select', 'cmb2' ),
-		'desc'             => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'               => $prefix . 'select',
-		'type'             => 'select',
-		'show_option_none' => true,
-		'options'          => array(
-			'standard' => esc_html__( 'Option One', 'cmb2' ),
-			'custom'   => esc_html__( 'Option Two', 'cmb2' ),
-			'none'     => esc_html__( 'Option Three', 'cmb2' ),
-		),
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'             => esc_html__( 'Test Radio inline', 'cmb2' ),
-		'desc'             => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'               => $prefix . 'radio_inline',
-		'type'             => 'radio_inline',
-		'show_option_none' => 'No Selection',
-		'options'          => array(
-			'standard' => esc_html__( 'Option One', 'cmb2' ),
-			'custom'   => esc_html__( 'Option Two', 'cmb2' ),
-			'none'     => esc_html__( 'Option Three', 'cmb2' ),
-		),
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'    => esc_html__( 'Test Radio', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'      => $prefix . 'radio',
-		'type'    => 'radio',
-		'options' => array(
-			'option1' => esc_html__( 'Option One', 'cmb2' ),
-			'option2' => esc_html__( 'Option Two', 'cmb2' ),
-			'option3' => esc_html__( 'Option Three', 'cmb2' ),
-		),
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'     => esc_html__( 'Test Taxonomy Radio', 'cmb2' ),
-		'desc'     => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'       => $prefix . 'text_taxonomy_radio',
-		'type'     => 'taxonomy_radio', // Or `taxonomy_radio_inline`/`taxonomy_radio_hierarchical`
-		'taxonomy' => 'category', // Taxonomy Slug
-		// 'inline'  => true, // Toggles display to inline
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'     => esc_html__( 'Test Taxonomy Select', 'cmb2' ),
-		'desc'     => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'       => $prefix . 'taxonomy_select',
-		'type'     => 'taxonomy_select',
-		'taxonomy' => 'category', // Taxonomy Slug
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'     => esc_html__( 'Test Taxonomy Multi Checkbox', 'cmb2' ),
-		'desc'     => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'       => $prefix . 'multitaxonomy',
-		'type'     => 'taxonomy_multicheck', // Or `taxonomy_multicheck_inline`/`taxonomy_multicheck_hierarchical`
-		'taxonomy' => 'post_tag', // Taxonomy Slug
-		// 'inline'  => true, // Toggles display to inline
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Checkbox', 'cmb2' ),
-		'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'   => $prefix . 'checkbox',
-		'type' => 'checkbox',
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'    => esc_html__( 'Test Multi Checkbox', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'      => $prefix . 'multicheckbox',
-		'type'    => 'multicheck',
-		// 'multiple' => true, // Store values in individual rows
-		'options' => array(
-			'check1' => esc_html__( 'Check One', 'cmb2' ),
-			'check2' => esc_html__( 'Check Two', 'cmb2' ),
-			'check3' => esc_html__( 'Check Three', 'cmb2' ),
-		),
-		// 'inline'  => true, // Toggles display to inline
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'    => esc_html__( 'Test wysiwyg', 'cmb2' ),
-		'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-		'id'      => $prefix . 'wysiwyg',
-		'type'    => 'wysiwyg',
-		'options' => array(
-			'textarea_rows' => 5,
-		),
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'Test Image', 'cmb2' ),
-		'desc' => esc_html__( 'Upload an image or enter a URL.', 'cmb2' ),
-		'id'   => $prefix . 'image',
-		'type' => 'file',
-	) );
-
-	$cmb_demo->add_field( array(
-		'name'         => esc_html__( 'Multiple Files', 'cmb2' ),
-		'desc'         => esc_html__( 'Upload or add multiple images/attachments.', 'cmb2' ),
-		'id'           => $prefix . 'file_list',
-		'type'         => 'file_list',
-		'preview_size' => array( 100, 100 ), // Default: array( 50, 50 )
-	) );
-
-	$cmb_demo->add_field( array(
-		'name' => esc_html__( 'oEmbed', 'cmb2' ),
-		'desc' => sprintf(
-			/* translators: %s: link to codex.wordpress.org/Embeds */
-			esc_html__( 'Enter a youtube, twitter, or instagram URL. Supports services listed at %s.', 'cmb2' ),
-			'<a href="https://codex.wordpress.org/Embeds">codex.wordpress.org/Embeds</a>'
-		),
-		'id'   => $prefix . 'embed',
-		'type' => 'oembed',
-	) );
+  $participants->add_field(array(
+    'name' => esc_html__('URL', 'cmb2'),
+    'desc' => esc_html__('Link a sitio personal', 'cmb2'),
+    'id' => $prefix . 'url',
+    'type' => 'text_url',
+  ));
 }
 
-add_action( 'cmb2_admin_init', 'bordes_register_repeatable_group_field_metabox' );
-/**
- * Hook in and add a metabox to demonstrate repeatable grouped fields
- */
-function bordes_register_repeatable_group_field_metabox() {
-	$prefix = 'bordes_group_';
-
-	/**
-	 * Repeatable Field Groups
-	 */
-	$cmb_group = new_cmb2_box( array(
-		'id'           => $prefix . 'metabox',
-		'title'        => esc_html__( 'Repeating Field Group', 'cmb2' ),
-		'object_types' => array( 'post' ),
-	) );
-
-	// $group_field_id is the field id string, so in this case: $prefix . 'demo'
-	$group_field_id = $cmb_group->add_field( array(
-		'id'          => $prefix . 'demo',
-		'type'        => 'group',
-		'description' => esc_html__( 'Generates reusable form entries', 'cmb2' ),
-		'options'     => array(
-			'group_title'   => esc_html__( 'Entry {#}', 'cmb2' ), // {#} gets replaced by row number
-			'add_button'    => esc_html__( 'Add Another Entry', 'cmb2' ),
-			'remove_button' => esc_html__( 'Remove Entry', 'cmb2' ),
-			'sortable'      => true,
-			// 'closed'     => true, // true to have the groups closed by default
-		),
-	) );
-
-	/**
-	 * Group fields works the same, except ids only need
-	 * to be unique to the group. Prefix is not needed.
-	 *
-	 * The parent field's id needs to be passed as the first argument.
-	 */
-	$cmb_group->add_group_field( $group_field_id, array(
-		'name'       => esc_html__( 'Entry Title', 'cmb2' ),
-		'id'         => 'title',
-		'type'       => 'text',
-		// 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
-	) );
-
-	$cmb_group->add_group_field( $group_field_id, array(
-		'name'        => esc_html__( 'Description', 'cmb2' ),
-		'description' => esc_html__( 'Write a short description for this entry', 'cmb2' ),
-		'id'          => 'description',
-		'type'        => 'textarea_small',
-	) );
-
-	$cmb_group->add_group_field( $group_field_id, array(
-		'name' => esc_html__( 'Entry Image', 'cmb2' ),
-		'id'   => 'image',
-		'type' => 'file',
-	) );
-
-	$cmb_group->add_group_field( $group_field_id, array(
-		'name' => esc_html__( 'Image Caption', 'cmb2' ),
-		'id'   => 'image_caption',
-		'type' => 'text',
-	) );
-
-}
+add_action('cmb2_admin_init', 'bordes_register_metabox');
