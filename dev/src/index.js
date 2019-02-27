1bf05ac8b62fccb6071325e9a185d92056756854
Juan Camilo González <info@juancgonzalez.com>
2019-02-26 22:30:34 -0500

    main scripts

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,2 +1,17 @@
 import sass from './scss/styles.scss';
+import Brain from './js/components/Brain';
 
+if (document.querySelector('.home')) {
+  const brain = new Brain(document.querySelectorAll('.tag'));
+  brain.loop();
+
+  let modal = document.getElementById('info');
+
+  document.onmouseup = e => {
+    if (modal.classList.contains('active')) {
+      if (e.target !== modal) {
+        modal.classList.remove('active');
+      }
+    }
+  };
+}
